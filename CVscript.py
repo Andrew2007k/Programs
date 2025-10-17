7a8116b62ec7fdd76709cf624e0a67380fe64dc9
Andrew Kurien <andrewkurien07@gmail.com>
2025-10-16 23:17:15 -0500

    Understanding the panda

--- CVscript.py
+++ CVscript.py
@@ -5,8 +5,16 @@ def CVAnalyze(filepath):
     with open(filepath, 'r') as DTA1:
         Datalines = DTA1.readlines()
     start = next(i for i, line in enumerate(Datalines) if line.strip().startswith("CURVE1"))
-    df = panda.read_csv(filepath, sep='\t', skiprows=start + 2, comment='#')
-    df.columns = ['Vf', 'Im']
-    df = df[['Vf', 'Im']].dropna()
+    df = panda.read_csv(filepath, sep=r'\s+', skiprows=start + 3, comment='#', header=None)
+    df.columns = ['A', 'Sec', 'Vf', 'Im', 'Col5', 'Col6', 'Col7', 'Col8', 'col9']
+    df = df[['Sec', 'Vf', 'Im']].dropna()
 
+    df['Vf'] = panda.to_numeric(df['Vf'], errors='coerce')
+    df['Im'] = panda.to_numeric(df['Im'], errors='coerce')
 
+    threshhold1 = 0
+    dIdV = np.gradient(df['Im'], df['Vf'])
+    rside = np.argmax(dIdV < threshhold1) * 0.2  #Fix this value to match scans
+    rside = round(rside)
+
+    
